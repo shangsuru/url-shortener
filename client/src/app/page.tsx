9e3f98cd26a2cfdca342a4bb01e97c39208f3eee
shangsuru <helm.henry99@gmail.com>
2023-08-13 02:14:30 +0900

    Handle margin of card outside of component

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,13 +2,15 @@ import Card from "@/components/Card";
 
 export default function Home() {
   return (
-    <Card
-      title="LinkShrink URL Shortener"
-      description="Create short & memorable links in seconds with this free tool."
-      inputPlaceholder="Enter link here"
-      buttonText="Shorten URL"
-      linkText="Show URL stats"
-      linkHref="/stats"
-    />
+    <div className="mt-20 sm:mx-20">
+      <Card
+        title="LinkShrink URL Shortener"
+        description="Create short & memorable links in seconds with this free tool."
+        inputPlaceholder="Enter link here"
+        buttonText="Shorten URL"
+        linkText="Show URL stats"
+        linkHref="/stats"
+      />
+    </div>
   );
 }
