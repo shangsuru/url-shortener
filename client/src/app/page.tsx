567482f3f9af5a9dc238581e58538335365e1c7f
shangsuru <helm.henry99@gmail.com>
2023-10-31 17:25:31 +0900

    Remove Card Component

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,17 +1,44 @@
-import Card from "@/components/Card";
+import { useState } from "react";
+
 import ShortURL from "@/components/ShortURL";
 
 export default function Home() {
+  function getShortUrl(longUrl: string) {
+    fetch(`localhost:8080/shorten?long=${longUrl}`).then((res) => {
+      res.json().then((data) => {
+        console.log(data);
+      });
+    });
+  }
+
   return (
     <div className="mt-20 sm:mx-20">
-      <Card
-        title="LinkShrink URL Shortener"
-        description="Create short & memorable links in seconds with this free tool."
-        inputPlaceholder="Enter link here"
-        buttonText="Shorten URL"
-        linkText="Show URL stats"
-        linkHref="/stats"
-      />
+      <div>
+        <div className="card max-w-[1500px] text-md md:text-xl lg:text-2xl 2xl:text-4xl">
+          <h1 className="font-bold flex justify-center items-center text-2xl md:text-4xl lg:text-5xl 2xl:text-6xl mb-10 lg:mb-20">
+            LinkShrink URL Shortener
+          </h1>
+          <p className="flex justify-center items-center text-center sm:text-md sm:text-xl lg:text-2xl 2xl:text-4xl mt-5 2xl:mt-10  text-gray-500">
+            Create short & memorable links in seconds with this free tool.
+          </p>
+
+          <div className="sm:flex sm:justify-center sm:items-center sm:flex-left mt-10 2xl:mt-20">
+            <input
+              placeholder="Enter link here"
+              className="px-4 py-2 block sm:w-2/3 lg:w-3/4 border border-grey-800 outline-none  focus:border-black mr-3 lg:mr-10"
+            />
+            <button className="font-bold mt-5 sm:mt-0 px-4 py-2 sm:w-1/3 lg:w-1/4 bg-blue-500 hover:bg-blue-700 text-white border border-blue-700 rounded">
+              Shorten URL
+            </button>
+          </div>
+
+          <div className="mt-10 2xl:mt-20 flex justify-center items-center">
+            <a href="/stats" className="link">
+              Show URL stats
+            </a>
+          </div>
+        </div>
+      </div>
       <div className="m-5">
         <ShortURL longURL="henryhelm.com" shortURL="linkshrink.io/rgzbyax" />
       </div>
--- client/src/app/stats/page.tsx
+++ client/src/app/stats/page.tsx
@@ -1,16 +1,37 @@
-import Card from "@/components/Card";
-
 export default function Stats() {
+  function getStats(shortUrl: string) {
+    console.log("TODO");
+  }
+
   return (
     <div className="mt-20 sm:mx-20">
-      <Card
-        title="Track your short URLs"
-        description="Enter the URL to find out how many clicks it has received so far, and other stuff."
-        inputPlaceholder="Enter your LinkShrink here"
-        buttonText="View Clicks"
-        linkText="Short another URL"
-        linkHref="/"
-      />
+      <div>
+        <div className="card max-w-[1500px] text-md md:text-xl lg:text-2xl 2xl:text-4xl">
+          <h1 className="font-bold flex justify-center items-center text-2xl md:text-4xl lg:text-5xl 2xl:text-6xl mb-10 lg:mb-20">
+            Track your short URLs
+          </h1>
+          <p className="flex justify-center items-center text-center sm:text-md sm:text-xl lg:text-2xl 2xl:text-4xl mt-5 2xl:mt-10  text-gray-500">
+            Enter the URL to find out how many clicks it has received so far,
+            and other stuff.
+          </p>
+
+          <div className="sm:flex sm:justify-center sm:items-center sm:flex-left mt-10 2xl:mt-20">
+            <input
+              placeholder="Enter your LinkShrink here"
+              className="px-4 py-2 block sm:w-2/3 lg:w-3/4 border border-grey-800 outline-none  focus:border-black mr-3 lg:mr-10"
+            />
+            <button className="font-bold mt-5 sm:mt-0 px-4 py-2 sm:w-1/3 lg:w-1/4 bg-blue-500 hover:bg-blue-700 text-white border border-blue-700 rounded">
+              View Clicks
+            </button>
+          </div>
+
+          <div className="mt-10 2xl:mt-20 flex justify-center items-center">
+            <a href="/" className="link">
+              Short another URL
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
