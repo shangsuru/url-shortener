76ba2ce39c82078be191f6817311cf50d90681b8
shangsuru <helm.henry99@gmail.com>
2023-11-07 17:56:37 +0900

    Add error alert and check for valid url client side

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,18 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import ShortURL from "@/components/ShortURL";
 import axios from "axios";
+
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref
+) {
+  return <MuiAlert elevation={10} ref={ref} variant="filled" {...props} />;
+});
 
 interface Url {
   long: string;
@@ -12,17 +22,57 @@ interface Url {
 export default function Home() {
   const [longUrl, setLongUrl] = useState("");
   const [createdUrls, setCreatedUrls] = useState<Url[]>([]);
+  const [showError, setShowError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   function getShortUrl(longUrl: string) {
+    // if it doesnt start with http:// or https://, add it
+    if (
+      !longUrl.startsWith("http://") &&
+      !longUrl.startsWith("https://") &&
+      longUrl !== ""
+    ) {
+      longUrl = "https://" + longUrl;
+    }
+    // check if url is valid URI
+    try {
+      new URL(longUrl);
+    } catch (err) {
+      setShowError(true);
+      setErrorMessage("Please enter a valid URL.");
+      return;
+    }
+
     axios
       .post(`${process.env.NEXT_PUBLIC_BACKEND_API}/shorten?long=${longUrl}`)
       .then((res) => {
+        if (res.data.error) {
+          setShowError(true);
+          setErrorMessage(res.data.error);
+          return;
+        }
         setCreatedUrls([...createdUrls, res.data]);
+      })
+      .catch((err) => {
+        setShowError(true);
+        setErrorMessage("Could not process request. Please try again.");
       });
+  }
+
+  function handleClose(event?: React.SyntheticEvent | Event, reason?: string) {
+    if (reason === "clickaway") {
+      return;
+    }
+    setShowError(false);
   }
 
   return (
     <div className="mt-20 sm:mx-20">
+      <Snackbar open={showError} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+          {errorMessage}
+        </Alert>
+      </Snackbar>
       <div>
         <div className="card max-w-[1500px] text-md md:text-xl lg:text-2xl 2xl:text-4xl">
           <h1 className="font-bold flex justify-center items-center text-2xl md:text-4xl lg:text-5xl 2xl:text-6xl mb-10 lg:mb-20">
